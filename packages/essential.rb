0dd807b84724d6bfbc118b11e65d30654a81fb62
Matthew Vince <matthew.vince@gmail.com>
2010-09-28 20:45:13 -0500

    Add aptitude update/grade on start

--- packages/essential.rb
+++ packages/essential.rb
@@ -1,4 +1,7 @@
 package :build_essential do
   describe 'Build tools'
-  apt 'build-dep build-essential zlib1g-dev libssl-dev libreadline5-dev libncurses5-dev file'
+  apt 'build-dep build-essential zlib1g-dev libssl-dev libreadline5-dev libncurses5-dev file' do
+    pre :install, 'apt-get -y update',
+                  'apt-get -y dist-upgrade'
+  end
 end
